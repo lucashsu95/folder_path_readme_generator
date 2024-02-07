c3892a2a3c68a46eae877d9b2814be9f334c53aa
lucashsu95 <lucashsu95@gmail.com>
2024-02-07 12:55:55 +0800

    Update

---
+++ demo.md
@@ -0,0 +1,23 @@
+- # 第1章 測式 
+
+    - # 第1章 aa 
+
+        - # 第1章 bb 
+
+            - 01-1 [cc.txt](測式/aa/bb/cc.txt)
+        - # 第1章 dd 
+
+            - 01-1 [a.txt](測式/aa/dd/a.txt)
+            - 01-2 [b.txt](測式/aa/dd/b.txt)
+    - # 第1章 bb 
+
+        - 01-1 [aa.txt](測式/bb/aa.txt)
+        - # 第1章 cc 
+
+            - 01-1 [a.txt](測式/bb/cc/a.txt)
+            - 01-2 [b.txt](測式/bb/cc/b.txt)
+            - 01-3 [c.txt](測式/bb/cc/c.txt)
+        - # 第1章 dd 
+
+        - # 第1章 ee 
+
--- folder_path_readme_generator.py
+++ folder_path_readme_generator.py
@@ -3,13 +3,10 @@
 import os
 
 def processStr(n:str):
-    new_st = ''
+    newSt = ''
     for i in n.replace(root_path,''): # 替換空格
-        if i == ' ':
-            new_st += '%20'
-        else:
-            new_st += i
-    return new_st[1:].replace("\\","/") # 去掉根目錄，把反斜線換成斜線
+        newSt += '%20' if i == ' ' else i
+    return newSt[1:].replace("\\","/") # 去掉根目錄，把反斜線換成斜線
 
 
 def dfs(directory, output, depth=1):
@@ -17,31 +14,31 @@ def dfs(directory, output, depth=1):
     try:
         items = os.listdir(directory)
         for item_idx, item in enumerate(items, start=1):
-            if item in ['.git','README.md','folder_path_readme_generator.py']:
+            if item in gitignore:
                 continue
             item_path = os.path.join(directory, item)
 
+            space = ' ' * (depth-1) * 4
             if os.path.isdir(item_path):
                 title = f"第{sec_id}章"
-                print(f"# {title} {item}", "\n", file=output)
+                print(f"{space}- # {title} {item}", "\n", file=output)
                 dfs(item_path, output, depth=depth + 1)
             else:
                 title = f"{sec_id:02d}-{item_idx}"
-                print(f"- {title} [{item}]({processStr(item_path)})", file=output)
+                print(f"{space}- {title} [{item}]({processStr(item_path)})", file=output)
 
             if depth == 1:
                 sec_id += 1
 
-        print("\n", file=output)
+        # print("\n", file=output)
 
     except OSError:
         print(f"Error reading files in directory: {directory}")
 
-
+gitignore = ['.git','README.md','folder_path_readme_generator.py','demo.md'] # 要忽略的檔案
 sec_id = 1
-
 folder_name = os.path.abspath('.')
 root_path = f'{folder_name}'
-output = open(f"{folder_name}/README.md", "w", encoding="utf-8")
+output = open(f"{folder_name}/demo.md", "w", encoding="utf-8")
 dfs(root_path, output)
 output.close()
