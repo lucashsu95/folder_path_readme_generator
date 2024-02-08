e985aedcd477e537774db912f93af1ab18c4f9e8
lucashsu95 <lucashsu95@gmail.com>
2024-02-08 23:30:00 +0800

    優化

--- folder_path_readme_generator.py
+++ folder_path_readme_generator.py
@@ -2,36 +2,28 @@
 
 import os
 
-def process_path(n:str):
-    path = ''
-    for i in n.replace(folder_name,''): # 替換空格
-        path += '%20' if i == ' ' else i
-    return path[1:].replace("\\","/") # 去掉根目錄，把反斜線換成斜線
-    
-
-# def process_path(path):
-#     """處理路徑，替換空格為 %20 並將反斜線換成斜線"""
-#     return path.replace(' ', '%20').replace("\\", "/")[1:]
+def process_path(name:str):
+    return '/'.join(name.replace(folder_name,'').replace(' ', '%20').split('\\'))[1:]
 
 
-def dfs(directory, output, folderIdxs,depth=1):
+def dfs(directory, output, folder_indexes,depth=1):
     try:
-        items = os.listdir(directory)
-        flag = 0
+        ignore_list = ('.git', 'README.md', 'demo.md', 'folder_path_readme_generator.py')
+        items = list(filter(lambda x: x not in ignore_list, os.listdir(directory)))
+        folder_index = 0
+
         for idx,item in enumerate(items,start=1):
-            if item in ('.git','README.md','demo.md','folder_path_readme_generator.py'): continue
             item_path = os.path.join(directory, item)
             space = ' ' * (depth - 1) * 4
 
             if os.path.isdir(item_path):
-                folderIdxs[-1] = str(int(folderIdxs[-1]) + 1)
-
-                print(f"{space}- {'#' * min(depth, 7)} 第{'-'.join(folderIdxs)}章 [{item}]({process_path(item_path)})", file=output)
+                folder_indexes[-1] = str(int(folder_indexes[-1]) + 1)
+                print(f"{space}- {'#' * min(depth, 6)} 第{'-'.join(folder_indexes)}章 [{item}]({process_path(item_path)})",file=output)
                 if depth < 9999:
-                    dfs(item_path, output,folderIdxs + [str(flag)], depth=depth + 1)
+                    dfs(item_path, output,folder_indexes + [str(folder_index)], depth + 1)
             else:
-                title = f"{'-'.join(folderIdxs[:-1])}_**{idx:02d}**"
-                print(f"{space}- {title} [{item}]({process_path(item_path)})", file=output)
+                title = f"{'-'.join(folder_indexes[:-1])}_**{idx:02d}**"
+                print(f"{space}- {title} [{item}]({process_path(item_path)})",file=output)
 
     except OSError:
         print(f"Error reading files in directory: {directory}")
